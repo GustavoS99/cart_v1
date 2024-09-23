8cf5ac4277733c8bf382068ffe53729e39fb69aa
Gustavo <desarrollador.gustavo@gmail.com>
2024-09-23 15:59:58 -0500

    Init Cart microservice

---
+++ src/main/java/com/emazon/cart_v1/CartV1Application.java
@@ -0,0 +1,13 @@
+package com.emazon.cart_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/cart_v1/CartV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.cart_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
